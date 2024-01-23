50e75722608a06ed630a7357e0d1f0d0884e1695
Tamir Sida <tamirsida@Tamirs-MacBook-Pro.local>
2024-01-23 16:28:49 +0200

    v2print

--- GameOfLife.java
+++ GameOfLife.java
@@ -146,18 +146,14 @@ public class GameOfLife {
 	
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
-		for(int i=1;i<arr.length-1;i++){
-			for(int j=1;j<arr[0].length-1;j++){
-				if (j < arr[i].length - 2) {
-					System.out.printf("%d  ", arr[i][j]); // Print with space
-				} else {
-					System.out.printf("%d", arr[i][j]); // Last element without space
-				}
+		for (int i = 1; i < arr.length - 1; i++) {
+			for (int j = 1; j < arr[i].length - 1; j++) {
+				System.out.print(arr[i][j] + "  "); // Print with space
 			}
-			System.out.println();
-			System.out.println();
+			System.out.println(); // Remove the extra line here
 		}
 	}
+	
 		
     // Displays the board. Living and dead cells are represented by black and white squares, respectively.
     // We use a fixed-size canvas of 900 pixels by 900 pixels for displaying game boards of different sizes.
